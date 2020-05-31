0d3c8d568c27c47fef5716a62497dd75def18fd4
FatMaus <57862769+FatMaus@users.noreply.github.com>
2020-05-31 22:06:46 +0800

    algorithm

---
+++ algorithm/bubbleSort.go
@@ -0,0 +1,18 @@
+package algorithm
+
+// BubbleSort 冒泡排序，复杂度O(N^2)
+func BubbleSort(numArr []int) {
+	// 排好n-1个元素则全体排列完毕
+	for i := 0; i < len(numArr)-1; i++ {
+		// 循环n-1次的冒泡(降序)
+		for n := 1; n < len(numArr); n++ {
+			if numArr[n-1] < numArr[n] {
+				var temp = numArr[n-1]
+				numArr[n-1] = numArr[n]
+				numArr[n] = temp
+			} else {
+				continue
+			}
+		}
+	}
+}
---
+++ algorithm/mergeSort.go
@@ -0,0 +1,43 @@
+package algorithm
+
+// MergeSort 归并排序，从中拆开二分，再分别递归至只有一个元素
+func MergeSort(numArr []int, start int, end int) {
+	// 拆至只有一个元素时停止
+	if end-start < 1 {
+		return
+	}
+	// 不断从中拆分递归
+	var middle int = (start + end) / 2
+	MergeSort(numArr, start, middle)
+	MergeSort(numArr, middle+1, end)
+	// 拆分后再排序
+	merge(numArr, start, middle, end)
+}
+
+// merge 排序函数，主要比较中位元素和两侧元素
+func merge(numArr []int, left int, mid int, right int) {
+	// 拷贝数组进入一个辅助数组，以免影响原数组的元素
+	var helpArr = make([]int, len(numArr), len(numArr))
+	copy(helpArr, numArr)
+	// 设置辅助索引，进行排序
+	var leftStart = left
+	var rightStart = mid + 1
+	for i := left; i <= right; i++ {
+		if leftStart > mid {
+			// 此时表明偶数个元素的数组已被拆至最简
+			numArr[i] = helpArr[rightStart]
+			rightStart++
+		} else if rightStart > right {
+			// 表明奇数个元素的数组已被拆至最简
+			numArr[i] = helpArr[leftStart]
+			leftStart++
+		} else if helpArr[leftStart] < helpArr[rightStart] {
+			// 进行排序，符合规矩则维持原样，否则调换位置
+			numArr[i] = helpArr[leftStart]
+			leftStart++
+		} else {
+			numArr[i] = helpArr[rightStart]
+			rightStart++
+		}
+	}
+}
